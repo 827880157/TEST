7059b3e3edf6886dbd4ed2fc54226a75522a52e5
chenzebo <827880157@qq.com>
2019-09-19 16:47:27 +0800

    third

--- src/main/java/com/example/demo/HelloSpringApplication.java
+++ src/main/java/com/example/demo/HelloSpringApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Spring");
+		System.out.println("Hel Spring");
 		SpringApplication.run(HelloSpringApplication.class, args);
 		
 	}
