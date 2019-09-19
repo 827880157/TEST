baecbd1997c494faf243122ba96eeaaff5c4ecd6
unknown <827880157@qq.com>
2019-09-19 15:43:46 +0800

    first commit

---
+++ src/main/java/com/example/demo/HelloSpringApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		System.out.println("ÄãºÃ Spring");
+		SpringApplication.run(HelloSpringApplication.class, args);
+		
+	}
+
+}
